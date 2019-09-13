0f9c00eceeaa9330487ffa9ddabcf5bbea6e6773
Tristan Peng <pengt@catlin.edu>
2019-09-12 18:08:10 -0700

    Get all other relevant information

--- script.js
+++ script.js
@@ -3,6 +3,38 @@ const inMatrix = (query, matrix) => {
   matrix.forEach((el, i) => { if (el.includes(query)) res = i; });
   return res;
 };
+
+const getDescription = matrix => {
+  let i = inMatrix('description', matrix);
+  let description = [];
+  if (i > -1) {
+    let raw = matrix[i][3];
+    description = raw.split('; ').map(b => {
+      let kv = b.split(': ');
+      if (kv.length > 2) {
+        for (let i = 2; i < kv.length; i++) kv[1] += `: ${ kv[i] }`;
+      }
+      return { [kv[0]]: kv[1] };
+    });
+  }
+  return description;
+};
+
+const getDT = (time, matrix) => {
+  let i = inMatrix(`dt${ time }`, matrix);
+  return i > -1 ? new Date(matrix[i][3]) : [];
+};
+
+const getLocation = matrix => {
+  let i = inMatrix('location', matrix);
+  return i > -1 ? matrix[i][3] : 'N/A';
+};
+
+const getSummary = matrix => {
+  let i = inMatrix('summary', matrix);
+  return i > -1 ? matrix[i][3].split(' - ') : ['N/A', 'N/A', 'N/A'];
+};
+
 window.addEventListener('load', () => {
   M.AutoInit();
   try {
@@ -13,18 +45,12 @@ window.addEventListener('load', () => {
     let events = ical[2];
     events.forEach(a => {
       if (a[1].length === 8) {
-        let i = inMatrix('description', a[1]);
-        let description = [];
-        if (i > -1) {
-          let raw = a[1][i][3];
-          description = raw.split('; ').map(b => {
-            let kv = b.split(': ');
-            if (kv.length > 2) {
-              for (let i = 2; i < kv.length; i++) kv[1] += `: ${ kv[i] }`;
-            }
-            return { [kv[0]]: kv[1] };
-          });
-        }
+        let description = getDescription(a[1]);
+        let dtstart = getDT('start', a[1]);
+        let dtend = getDT('end', a[1]);
+        let location = getLocation(a[1]);
+        let summary = getSummary(a[1]);
+        console.log(summary);
       }
     });
   } catch {
