397e5c5afc2e0a6db866200ddf766d58a0ec3701
Tristan Peng <pengt@catlin.edu>
2019-09-15 12:54:29 -0700

    Add unique classes to every day

--- index.php
+++ index.php
@@ -73,99 +73,99 @@
           </tr>
           <tr class="mins45">
             <td class="times mins45">8:00-8:45</td>
-            <td rowspan="1" class="period mins45" id="0-0">Free</td>
-            <td rowspan="1" class="period mins45" id="1-0">Free</td>
-            <td rowspan="1" class="period mins45" id="2-0">Free</td>
-            <td rowspan="1" class="period mins45" id="3-0">Free</td>
-            <td rowspan="1" class="period mins45" id="4-0">Free</td>
+            <td rowspan="1" class="period mins45 0" id="0-0">Free</td>
+            <td rowspan="1" class="period mins45 1" id="1-0">Free</td>
+            <td rowspan="1" class="period mins45 2" id="2-0">Free</td>
+            <td rowspan="1" class="period mins45 3" id="3-0">Free</td>
+            <td rowspan="1" class="period mins45 4" id="4-0">Free</td>
           </tr>
           <tr class="mins25">
             <td class="times mins25">8:45-9:15</td>
-            <td rowspan="1" class="period mins25" id="0-1">Free<span class="subtitle"> - Early Flex</span></td>
-            <td rowspan="1" class="period mins25" id="1-1">Free<span class="subtitle"> - Early Flex</span></td>
-            <td rowspan="1" class="period mins25" id="2-1">Free<span class="subtitle"> - Early Flex</span></td>
-            <td rowspan="1" class="period mins25" id="3-1">Free<span class="subtitle"> - Early Flex</span></td>
-            <td rowspan="1" class="period mins25" id="4-1">Free<span class="subtitle"> - Early Flex</span></td>
+            <td rowspan="1" class="period mins25 0" id="0-1">Free<span class="subtitle"> - Early Flex</span></td>
+            <td rowspan="1" class="period mins25 1" id="1-1">Free<span class="subtitle"> - Early Flex</span></td>
+            <td rowspan="1" class="period mins25 2" id="2-1">Free<span class="subtitle"> - Early Flex</span></td>
+            <td rowspan="1" class="period mins25 3" id="3-1">Free<span class="subtitle"> - Early Flex</span></td>
+            <td rowspan="1" class="period mins25 4" id="4-1">Free<span class="subtitle"> - Early Flex</span></td>
           </tr>
           <tr class="mins10">
             <td class="times mins10">9:20-9:30</td>
-            <td rowspan="1" class="period mins10" id="0-2">Break</td>
-            <td rowspan="1" class="period mins10" id="1-2">Break</td>
-            <td rowspan="1" class="period mins10" id="2-2">Break</td>
-            <td rowspan="1" class="period mins10" id="3-2">Break</td>
-            <td rowspan="1" class="period mins10" id="4-2">Break</td>
+            <td rowspan="1" class="period mins10 0" id="0-2">Break</td>
+            <td rowspan="1" class="period mins10 1" id="1-2">Break</td>
+            <td rowspan="1" class="period mins10 2" id="2-2">Break</td>
+            <td rowspan="1" class="period mins10 3" id="3-2">Break</td>
+            <td rowspan="1" class="period mins10 4" id="4-2">Break</td>
           </tr>
           <tr class="mins10">
             <td class="times mins10">9:30-9:40</td>
-            <td rowspan="1" class="period mins10" id="0-3">Break</td>
-            <td rowspan="1" class="period mins10" id="1-3">Break</td>
-            <td rowspan="1" class="period mins10" id="2-3">Break</td>
-            <td rowspan="1" class="period mins10" id="3-3">Break</td>
-            <td rowspan="1" class="period mins10" id="4-3">Break</td>
+            <td rowspan="1" class="period mins10 0" id="0-3">Break</td>
+            <td rowspan="1" class="period mins10 1" id="1-3">Break</td>
+            <td rowspan="1" class="period mins10 2" id="2-3">Break</td>
+            <td rowspan="1" class="period mins10 3" id="3-3">Break</td>
+            <td rowspan="1" class="period mins10 4" id="4-3">Break</td>
           </tr>
           <tr class="mins45">
             <td class="times mins45">9:45-10:30</td>
-            <td rowspan="1" class="period mins45" id="0-4">Free</td>
-            <td rowspan="1" class="period mins45" id="1-4">Free</td>
-            <td rowspan="1" class="period mins45" id="2-4">Free</td>
-            <td rowspan="1" class="period mins45" id="3-4">Free</td>
-            <td rowspan="1" class="period mins45" id="4-4">Free</td>
+            <td rowspan="1" class="period mins45 0" id="0-4">Free</td>
+            <td rowspan="1" class="period mins45 1" id="1-4">Free</td>
+            <td rowspan="1" class="period mins45 2" id="2-4">Free</td>
+            <td rowspan="1" class="period mins45 3" id="3-4">Free</td>
+            <td rowspan="1" class="period mins45 4" id="4-4">Free</td>
           </tr>
           <tr class="mins45">
             <td class="times mins45">10:35-11:20</td>
-            <td rowspan="1" class="period mins45" id="0-5">Free</td>
-            <td rowspan="1" class="period mins45" id="1-5">Free</td>
-            <td rowspan="1" class="period mins45" id="2-5">Free</td>
-            <td rowspan="1" class="period mins45" id="3-5">Free</td>
-            <td rowspan="1" class="period mins45" id="4-5">Free</td>
+            <td rowspan="1" class="period mins45 0" id="0-5">Free</td>
+            <td rowspan="1" class="period mins45 1" id="1-5">Free</td>
+            <td rowspan="1" class="period mins45 2" id="2-5">Free</td>
+            <td rowspan="1" class="period mins45 3" id="3-5">Free</td>
+            <td rowspan="1" class="period mins45 4" id="4-5">Free</td>
           </tr>
           <tr class="mins25">
             <td class="times mins25">11:20-11:50</td>
-            <td rowspan="1" class="period mins25" id="0-6">Free<span class="subtitle"> - AM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="1-6">Free<span class="subtitle"> - AM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="2-6">Free<span class="subtitle"> - AM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="3-6">Free<span class="subtitle"> - AM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="4-6">Free<span class="subtitle"> - AM Flex</span></td>
+            <td rowspan="1" class="period mins25 0" id="0-6">Free<span class="subtitle"> - AM Flex</span></td>
+            <td rowspan="1" class="period mins25 1" id="1-6">Free<span class="subtitle"> - AM Flex</span></td>
+            <td rowspan="1" class="period mins25 2" id="2-6">Free<span class="subtitle"> - AM Flex</span></td>
+            <td rowspan="1" class="period mins25 3" id="3-6">Free<span class="subtitle"> - AM Flex</span></td>
+            <td rowspan="1" class="period mins25 4" id="4-6">Free<span class="subtitle"> - AM Flex</span></td>
           </tr>
           <tr class="mins35">
             <td class="times mins35">11:55-12:30</td>
-            <td rowspan="1" class="period mins35" id="0-7">Co-Curric</td>
-            <td rowspan="1" class="period mins35" id="1-7">Co-Curric</td>
-            <td rowspan="1" class="period mins35" id="2-7">Co-Curric</td>
-            <td rowspan="1" class="period mins35" id="3-7">Co-Curric</td>
-            <td rowspan="1" class="period mins35" id="4-7">Co-Curric</td>
+            <td rowspan="1" class="period mins35 0" id="0-7">Co-Curric</td>
+            <td rowspan="1" class="period mins35 1" id="1-7">Co-Curric</td>
+            <td rowspan="1" class="period mins35 2" id="2-7">Co-Curric</td>
+            <td rowspan="1" class="period mins35 3" id="3-7">Co-Curric</td>
+            <td rowspan="1" class="period mins35 4" id="4-7">Co-Curric</td>
           </tr>
           <tr class="mins35">
             <td class="times mins35">12:30-1:05</td>
-            <td rowspan="1" class="period mins35" id="0-8">Lunch</td>
-            <td rowspan="1" class="period mins35" id="1-8">Lunch</td>
-            <td rowspan="1" class="period mins35" id="2-8">Lunch</td>
-            <td rowspan="1" class="period mins35" id="3-8">Lunch</td>
-            <td rowspan="1" class="period mins35" id="4-8">Lunch</td>
+            <td rowspan="1" class="period mins35 0" id="0-8">Lunch</td>
+            <td rowspan="1" class="period mins35 1" id="1-8">Lunch</td>
+            <td rowspan="1" class="period mins35 2" id="2-8">Lunch</td>
+            <td rowspan="1" class="period mins35 3" id="3-8">Lunch</td>
+            <td rowspan="1" class="period mins35 4" id="4-8">Lunch</td>
           </tr>
           <tr class="mins25">
             <td class="times mins25">1:10-1:40</td>
-            <td rowspan="1" class="period mins25" id="0-9">Free<span class="subtitle"> - PM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="1-9">Free<span class="subtitle"> - PM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="2-9">Free<span class="subtitle"> - PM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="3-9">Free<span class="subtitle"> - PM Flex</span></td>
-            <td rowspan="1" class="period mins25" id="4-9">Free<span class="subtitle"> - PM Flex</span></td>
+            <td rowspan="1" class="period mins25 0" id="0-9">Free<span class="subtitle"> - PM Flex</span></td>
+            <td rowspan="1" class="period mins25 1" id="1-9">Free<span class="subtitle"> - PM Flex</span></td>
+            <td rowspan="1" class="period mins25 2" id="2-9">Free<span class="subtitle"> - PM Flex</span></td>
+            <td rowspan="1" class="period mins25 3" id="3-9">Free<span class="subtitle"> - PM Flex</span></td>
+            <td rowspan="1" class="period mins25 4" id="4-9">Free<span class="subtitle"> - PM Flex</span></td>
           </tr>
           <tr class="mins45">
             <td class="times mins45">1:40-2:25</td>
-            <td rowspan="1" class="period mins45" id="0-10">Free</td>
-            <td rowspan="1" class="period mins45" id="1-10">Free</td>
-            <td rowspan="1" class="period mins45" id="2-10">Free</td>
-            <td rowspan="1" class="period mins45" id="3-10">Free</td>
-            <td rowspan="1" class="period mins45" id="4-10">Free</td>
+            <td rowspan="1" class="period mins45 0" id="0-10">Free</td>
+            <td rowspan="1" class="period mins45 1" id="1-10">Free</td>
+            <td rowspan="1" class="period mins45 2" id="2-10">Free</td>
+            <td rowspan="1" class="period mins45 3" id="3-10">Free</td>
+            <td rowspan="1" class="period mins45 4" id="4-10">Free</td>
           </tr>
           <tr class="mins45">
             <td class="times mins45">2:30-3:15</td>
-            <td rowspan="1" class="period mins45" id="0-11">Free</td>
-            <td rowspan="1" class="period mins45" id="1-11">Free</td>
-            <td rowspan="1" class="period mins45" id="2-11">Free</td>
-            <td rowspan="1" class="period mins45" id="3-11">Free</td>
-            <td rowspan="1" class="period mins45" id="4-11">Free</td>
+            <td rowspan="1" class="period mins45 0" id="0-11">Free</td>
+            <td rowspan="1" class="period mins45 1" id="1-11">Free</td>
+            <td rowspan="1" class="period mins45 2" id="2-11">Free</td>
+            <td rowspan="1" class="period mins45 3" id="3-11">Free</td>
+            <td rowspan="1" class="period mins45 4" id="4-11">Free</td>
           </tr>
         </tbody>
       </table>
